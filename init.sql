37e20207c30e815bf5c4c8e6c6468f5ac2570a39
cjp <570165887@qq.com>
2021-05-16 22:21:57 +0800

    添加docker-composer支持

---
+++ init.sql
@@ -0,0 +1,11 @@
+-- 创建数据库
+create database IF NOT EXISTS `docker_mysql` default character set utf8 collate utf8_general_ci;
+-- 创建用户权限
+use mysql;
+select host, user from user;
+-- 因为mysql版本是5.7，因此新建用户为如下命令：
+create user docker identified by '123456';
+-- 将docker_mysql数据库的权限授权给创建的docker用户，密码为123456：
+grant all on docker_mysql.* to docker@'%' identified by '123456' with grant option;
+-- 这一条命令一定要有：
+flush privileges;
