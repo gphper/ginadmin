f519fa8de1c09a7d47458e1a3d3a59caa1b8a321
gphper <570165887@qq.com>
2022-04-10 19:17:42 +0800

    设置时区

--- internal/router/default.go
+++ internal/router/default.go
@@ -38,7 +38,7 @@ func Init() *gin.Engine {
 
 	// router.Use(medium.GinLog(facade.NewZaplog("admin"), time.RFC3339, true), medium.RecoveryWithLog(facade.NewZaplog("admin"), true))
 	// router.Use(medium.GinLog(facade.NewRedisLog("admin"), time.RFC3339, true), medium.RecoveryWithLog(facade.NewRedisLog("admin"), true))
-	router.Use(medium.GinLog(facade.NewLogger("admin"), time.RFC3339, true), medium.RecoveryWithLog(facade.NewLogger("admin"), true))
+	router.Use(medium.GinLog(facade.NewLogger("admin"), time.RFC3339, false), medium.RecoveryWithLog(facade.NewLogger("admin"), true))
 	/*****admin路由定义******/
 	adminRouter := router.Group("/admin")
 
